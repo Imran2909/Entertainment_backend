28a3ee78a92a1bbfa1d3b55a66b6636a227ad5d0
Imran Sutar <112754652+Imran2909@users.noreply.github.com>
2024-08-03 19:25:59 +0530

    Update google-oauth.js
    Updated oauth link

--- config/google-oauth.js
+++ config/google-oauth.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:8050/auth/google/callback",
+  callbackURL: process.env.OAUTH_CALLBACK_URL,
   scope: ['email', 'profile']
   
 },
